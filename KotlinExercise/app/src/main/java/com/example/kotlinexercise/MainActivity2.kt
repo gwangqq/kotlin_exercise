6f7a26fe3fab79c6118c559a16d4b0f2981a36b4
gwanggyu park <pgg961@gmail.com>
2020-12-23 17:48:50 +0900

    kotlin extension exercise

--- KotlinExercise/app/src/main/java/com/example/kotlinexercise/MainActivity2.kt
+++ KotlinExercise/app/src/main/java/com/example/kotlinexercise/MainActivity2.kt
@@ -8,10 +8,13 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        // 
+
+        // String extension
         testStringExtension()
-        Child().sayHello()
     }
 
+    // String extension test
     fun testStringExtension() {
         var original = "Hello"
         var added = "Guys~"
@@ -24,15 +27,45 @@ fun String.plus(word: String): String {
     return this + word
 }
 
-open class Parent {
-    var testStr: String = "hello"
-    open fun sayHello() {
-        Log.d("inheritance", "${testStr}")
+// Extension Test
+open class Parent{
+    var greeting : String = "hello"
+    fun sayHello(){
+        Log.d("extension(Parent)", "${greeting}")
     }
 }
 
-class Child : Parent() {
-    override fun sayHello() {
-        Log.d("inheritance", "no, no, no")
+class Child : Parent(){
+    fun myHello(){
+        greeting = "Hi"
+        Log.d("extension(Child)", "${greeting}")
     }
 }
+
+// Method Override Exercise
+open class BaseClass {
+    open fun opened(){
+        Log.d("override", "This is BaseClass")
+    }
+    fun notOpened(){
+
+    }
+}
+
+class ChildClass : BaseClass(){
+    override fun opened() {
+        Log.d("override", "This is ChildClass")
+    }
+
+}
+
+open class BaseClass2{
+    open var opened : String = "I am"
+}
+
+class ChildClass2 : BaseClass2(){
+    override var opened: String = "you are"
+}
+
+
+
